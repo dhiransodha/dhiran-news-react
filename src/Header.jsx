a101834f484c9a69c89688d5c4e6309324cee282
dizzlesticks <sodha.dhiran@gmail.com>
2024-12-20 15:22:57 +0000

    add ability to register user

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,17 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export const Header = ({ user }) => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Image
-          src={user.avatar_url}
-          className="img-fluid"
-          style={{ height: "40px", width: "40px", paddingRight: "20px" }}
-          roundedCircle
-        />
+        {user.username ? (
+          <Link to="/profile">
+            {user.avatar_url ? (
+              <Image
+                src={user.avatar_url}
+                className="img-fluid"
+                style={{ maxHeight: "40px", paddingRight: "20px" }}
+                roundedCircle
+              />
+            ) : (
+              <>
+                <p>Profile</p>
+              </>
+            )}
+          </Link>
+        ) : null}
+
         <Navbar.Brand href="/">THE NEWS APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -23,7 +35,10 @@ export const Header = ({ user }) => {
             {user.username ? (
               <Nav.Link href="/sign-out">Sign out</Nav.Link>
             ) : (
-              <Nav.Link href="/sign-in">Sign in</Nav.Link>
+              <>
+                <Nav.Link href="/sign-in">Sign in</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+              </>
             )}
           </Nav>
         </Navbar.Collapse>
